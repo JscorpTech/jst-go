b2844e8071891f6fadcaa27794f61feee425d30d
A'zamov Samandar <admin@jscorp.uz>
2025-04-05 16:38:16 +0500

    Fayl yozish funksiyasini yaxshilash va marshrutlarni qo'shish

--- go-generate/internal/utils/file.go
+++ go-generate/internal/utils/file.go
@@ -15,11 +15,11 @@ func NewFile() domain.FileUtilPort {
 }
 
 func (f *file) Write(filePath string, content []byte) error {
-	// _, err := os.Stat(filePath)
-	// if !os.IsNotExist(err) {
-	// 	panic("Fayil mavjud: " + filePath)
-	// }
-	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	_, err := os.Stat(filePath)
+	if !os.IsNotExist(err) {
+		panic("Fayil mavjud: " + filePath)
+	}
+	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Print("Fayil yaratishda xatolik yuz berdi: " + filePath)
 	}
